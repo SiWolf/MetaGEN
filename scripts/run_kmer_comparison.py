ebe8fdfe847ae98f66b0a4e382429ec9b6741360
SiWolf <silveranthony@hotmail.de>
2023-03-29 14:28:21 +0200

    Updated kmer analysis script, will now only export highly abundant, differential kmers

--- scripts/run_kmer_comparison.py
+++ scripts/run_kmer_comparison.py
@@ -1,34 +1,48 @@
 # -------------------------------
 # Title: run_kmer_comparsion.py
 # Author: Silver A. Wolf
-# Last Modified: Sun, 26.03.2023
-# Version: 0.0.1
+# Last Modified: Wed, 29.03.2023
+# Version: 0.0.2
 # -------------------------------
 
 import os
+threads = "216"
+kmer_threshold = "254"
 
 # Identification of unique kmers per timepoint
 
 # t0: unique kmers to SSG
-os.system("kmc_tools complex scripts/kmer_comparisons/SSG_t0.txt -t 216")
-#kmc_dump output/03_functional_analysis/kmc3/{wildcards.sample} {output.kmc_txt}
+os.system("kmc_tools complex scripts/kmer_comparisons/SSG_t0.txt -t " + threads)
+os.system("kmc_dump tmp/unique_SSG_t0 tmp/unique_SSG_t0.txt")
+os.system("awk '{ if ($2 > " + kmer_threshold + ") { print } }' tmp/unique_SSG_t0.txt > output/03_functional_analysis/kmc3/unique_filter_SSG_t0.txt")
+os.system("rm tmp/unique_*")
 
 # t0: unique kmers to 5DG
-#os.system("kmc_tools complex scripts/kmer_comparisons/5DG_t0.txt -t 216")
-#kmc_dump output/03_functional_analysis/kmc3/{wildcards.sample} {output.kmc_txt}
+os.system("kmc_tools complex scripts/kmer_comparisons/5DG_t0.txt -t " + threads)
+os.system("kmc_dump tmp/unique_5DG_t0 tmp/unique_5DG_t0.txt")
+os.system("awk '{ if ($2 > " + kmer_threshold + ") { print } }' tmp/unique_5DG_t0.txt > output/03_functional_analysis/kmc3/unique_filter_5DG_t0.txt")
+os.system("rm tmp/unique_*")
 
 # t1: unique kmers to SSG
-#os.system("kmc_tools complex scripts/kmer_comparisons/SSG_t1.txt -t 216")
-#kmc_dump output/03_functional_analysis/kmc3/{wildcards.sample} {output.kmc_txt}
+os.system("kmc_tools complex scripts/kmer_comparisons/SSG_t1.txt -t " + threads)
+os.system("kmc_dump tmp/unique_SSG_t1 tmp/unique_SSG_t1.txt")
+os.system("awk '{ if ($2 > " + kmer_threshold + ") { print } }' tmp/unique_SSG_t1.txt > output/03_functional_analysis/kmc3/unique_filter_SSG_t1.txt")
+os.system("rm tmp/unique_*")
 
 # t1: unique kmers to 5DG
-#os.system("kmc_tools complex scripts/kmer_comparisons/5DG_t1.txt -t 216")
-#kmc_dump output/03_functional_analysis/kmc3/{wildcards.sample} {output.kmc_txt}
+os.system("kmc_tools complex scripts/kmer_comparisons/5DG_t1.txt -t " + threads)
+os.system("kmc_dump tmp/unique_5DG_t1 tmp/unique_5DG_t1.txt")
+os.system("awk '{ if ($2 > " + kmer_threshold + ") { print } }' tmp/unique_5DG_t1.txt > output/03_functional_analysis/kmc3/unique_filter_5DG_t1.txt")
+os.system("rm tmp/unique_*")
 
 # t2: unique kmers to SSG
-#os.system("kmc_tools complex scripts/kmer_comparisons/SSG_t2.txt -t 216")
-#kmc_dump output/03_functional_analysis/kmc3/{wildcards.sample} {output.kmc_txt}
+os.system("kmc_tools complex scripts/kmer_comparisons/SSG_t2.txt -t " + threads)
+os.system("kmc_dump tmp/unique_SSG_t2 tmp/unique_SSG_t2.txt")
+os.system("awk '{ if ($2 > " + kmer_threshold + ") { print } }' tmp/unique_SSG_t2.txt > output/03_functional_analysis/kmc3/unique_filter_SSG_t2.txt")
+os.system("rm tmp/unique_*")
 
 # t2: unique kmers to 5DG
-#os.system("kmc_tools complex scripts/kmer_comparisons/5DG_t2.txt -t 216")
-#kmc_dump output/03_functional_analysis/kmc3/{wildcards.sample} {output.kmc_txt}
+os.system("kmc_tools complex scripts/kmer_comparisons/5DG_t2.txt -t " + threads)
+os.system("kmc_dump tmp/unique_5DG_t2 tmp/unique_5DG_t2.txt")
+os.system("awk '{ if ($2 > " + kmer_threshold + ") { print } }' tmp/unique_5DG_t2.txt > output/03_functional_analysis/kmc3/unique_filter_5DG_t2.txt")
+os.system("rm tmp/unique_*")
